29a5a1d020ff85f3cabccb79f9adf75dfe0c2bd0
Martin Schilliger <martin_schilliger@bluewin.ch>
2019-07-02 09:44:55 +0200

    Initial commit

---
+++ stream.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Dieses Skript wird mit durch die Datei /etc/rc.local ausgeführt beim Start!
+
+source kamerasau.conf
+
+echo "Starting Stream to $RTMP_URL"
+
+raspivid -o - -t 0 --rotation 180 --vstab -w $WIDTH -h $HEIGHT -fps $FPS -b $BITRATE -g $KEYFRAME | \
+	ffmpeg \
+  -re -f h264 -i - -vcodec copy \
+  -an \
+  -g $KEYFRAME -strict experimental \
+  -tune zerolatency \
+  -preset $QUAL -flags +global_header \
+  -maxrate 1.5M -bufsize 3M \
+  -loglevel $LOGLEVEL \
+	-f flv $RTMP_URL/$STREAM_KEY
+
+echo "Finished streaming"
