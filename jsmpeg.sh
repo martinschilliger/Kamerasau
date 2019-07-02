99bc95d8188aea6fb4e6328de2f456fdc5b4e00a
Martin Schilliger <martin_schilliger@bluewin.ch>
2019-07-02 15:13:28 +0200

    Optimierungen

--- jsmpeg.sh
+++ jsmpeg.sh
@@ -6,10 +6,9 @@ echo "Starting Stream to $RTMP_URL"
 
 ffmpeg \
   -f v4l2 \
-    -framerate $FPS -video_size 640x480 -i /dev/video0 \
+    -framerate $FPS -vf "vflip,hflip" -video_size ${WIDTH}x${HEIGHT} -i /dev/video0 \
   -f mpegts \
-    -codec:v mpeg1video -s 640x480 -b:v $VBR -bf 0 \
-  -tune zerolatency \
+    -codec:v mpeg1video -s ${WIDTH}x${HEIGHT} -b:v $VBR -bf 0 \
   -loglevel $LOGLEVEL \
   $WEBSOCKETS_URL/$SECRET
 
