8ec8b901de60a8ad8eb512d76f41c66389d03201
Martin Schilliger <martin_schilliger@bluewin.ch>
2019-07-09 14:11:02 +0200

    Removed rotation from ffmpeg

--- jsmpeg.sh
+++ jsmpeg.sh
@@ -14,7 +14,7 @@ ffmpeg \
   -f v4l2 \
     -framerate $FPS -video_size ${WIDTH}x${HEIGHT} -i /dev/video0 \
   -f mpegts \
-     -vf "vflip,hflip" -codec:v mpeg1video -s ${WIDTH}x${HEIGHT} -b:v $VBR -bf 0 \
+     -codec:v mpeg1video -s ${WIDTH}x${HEIGHT} -b:v $VBR -bf 0 \
   -loglevel $LOGLEVEL \
   $WEBSOCKETS_URL/$SECRET
 
